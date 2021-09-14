c4716c5a75b61528accef260a8e9b5d8054e5c98
Kihungi <wambeliza@gmail.com>
2021-09-14 00:20:27 -0700

    CHANGES TEST CHALLENGE1

--- src/test_estimator.py
+++ src/test_estimator.py
@@ -3,8 +3,11 @@ import estimator
 
 class testEstimator(unittest.TestCase):
     def test_data(self):
-        result = estimator.CovidEstimator().estimator(28)
-        self.assertEqual(result,{'currentlyInfected1': 6740, 'infectionsByRequestedTime1': 3450880, 'currentlyInfected2': 33700,
+        result1 = estimator.CovidEstimator().estimator(28)
+        self.assertEqual(result1,{'currentlyInfected1': 6740, 'infectionsByRequestedTime1': 3450880, 'currentlyInfected2': 33700,
+       'infectionsByRequestedTime2': 17254400})
+        result2 = estimator.CovidEstimator().estimator(29)
+        self.assertEqual(result2,{'currentlyInfected1': 6740, 'infectionsByRequestedTime1': 3450880, 'currentlyInfected2': 33700,
 'infectionsByRequestedTime2': 17254400})
         
 if __name__ == "__main__":
